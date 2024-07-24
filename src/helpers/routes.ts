e4acd31ccd256d8a01c5ee1bb0475ec461271f1f
JuanPabloArias803 <juanpabloarias803@gmail.com>
2024-07-24 12:22:56 -0500

    set not found page

---
+++ src/helpers/routes.ts
@@ -0,0 +1,8 @@
+import { NotFound } from '../views/not-found/not-found';
+
+
+export const routes = {
+  public: [
+    { path: '/not-found', page: NotFound }
+  ],
+};
---
+++ src/views/not-found/not-found.ts
@@ -0,0 +1,10 @@
+import './not-found.css';
+
+export function NotFound(){
+    const $root=document.querySelector("#app") as HTMLDivElement;
+    $root.innerHTML=`
+        <div class="not-found-container">
+            <h2>Lo sentimos, la página no fue encontrada</h2>
+        </div>
+    `;
+}
